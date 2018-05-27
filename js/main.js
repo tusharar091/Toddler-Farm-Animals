39576e4ec74f9d96ca43407d7c51f521d93eb56f
tusharar091 <tusharar091@gmail.com>
2018-05-27 19:47:14 +0530

    All About grouping the elements together using add.group()

--- js/main.js
+++ js/main.js
@@ -13,13 +13,14 @@ var GameState={
     },  
     create:function(){
         this.background=this.game.add.sprite(0,0,'bg');
-        this.pig=this.game.add.sprite(this.game.world.centerX,this.game.world.centerY,'pig');
-        this.pig.anchor.setTo(0.5,0.5);
         
-        //userInput for pig
-        this.pig.inputEnabled=true;
-        this.pig.input.pixelPerfectClick=true;
-        this.pig.events.onInputDown.add(this.animateAnimal,this);
+//        this.pig=this.game.add.sprite(this.game.world.centerX,this.game.world.centerY,'pig');
+//        this.pig.anchor.setTo(0.5,0.5);
+//        
+//        //userInput for pig
+//        this.pig.inputEnabled=true;
+//        this.pig.input.pixelPerfectClick=true;
+//        this.pig.events.onInputDown.add(this.animateAnimal,this);
         
         //right arrow
         this.rightArrow=this.game.add.sprite(580,this.game.world.centerY,'arrow');
@@ -44,9 +45,34 @@ var GameState={
         this.leftArrow.input.pixelPerfectClick=true;
         this.leftArrow.events.onInputDown.add(this.switchAnimal,this);
         
-        //custom param for direction assigned to rightArrow to move object from left to right
+        //custom param for direction assigned to leftArrow to move object from left to right
 
         this.leftArrow.customParams= {direction: -1};
+        
+        var animalData=[{key:'chicken',text:'CHICKEN'},{key:'pig',text:'PIG'},{key:'horse',text:'HORSE'},{key:'sheep',text:'SHEEP'}];
+        
+        //creating a group named animals
+        this.animals=this.game.add.group();
+        var self=this;
+        
+        var animal;
+        //iterating over animal data to produce a set of sprites by using their kry value which represents the alias for the path they belong to
+        animalData.forEach(function(element){
+            animal=self.animals.create(-1000,self.game.world.centerY,element.key);
+            animal.anchor.setTo(0.5);
+            animal.customParams={text :  element.text};
+            animal.inputEnabled=true;
+            animal.input.pixelPerfectClick=true;
+            animal.events.onInputDown.add(self.animateAnimal,self);
+        
+            
+            
+            
+        });
+        this.currentAnimal=this.animals.next();
+        this.currentAnimal.position.set(game.world.centerX,game.world.centerY);
+        
+        
         
         
         
